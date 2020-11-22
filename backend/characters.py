ffd624d9e1a4a1bda0a44362cee4ff362ff14d67
Giulio <giulio97.migani@gmail.com>
2020-11-23 00:15:01 +0100

    added descriptions of the remaining characters

--- backend/characters.py
+++ backend/characters.py
@@ -32,6 +32,7 @@ class BartCassidy(Character):
     def __init__(self):
         super().__init__("Bart Cassidy", max_lives=4)
         self.desc = "Ogni volta che viene ferito, pesca una carta"
+        #una sola carta per ogni gruppo di danni. es: dinamite -> 1 carta
     
     def on_hurt(self, dmg):
         pass
@@ -53,7 +54,7 @@ class CalamityJanet(Character):
 class ElGringo(Character):
     def __init__(self):
         super().__init__("El Gringo", max_lives=3)
-        self.desc = "Ogni volta che perde un punto vita pesca una carta dalla mano del personaggio che gli ha sparato (solo se ha carte in mano; una carta per ogni punto vita)"
+        self.desc = "Ogni volta che perde un punto vita pesca una carta dalla mano del giocatore responsabile ma solo se il giocatore in questione ha carte in mano (una carta per ogni punto vita)"
         # ovviamente la dinamite non è considerata danno inferto da un giocatore
 
 class JesseJones(Character):
@@ -69,43 +70,54 @@ class Jourdonnais(Character):
 class KitCarlson(Character):
     def __init__(self):
         super().__init__("Kit Carlson", max_lives=4)
+        self.desc = "All'inizio del suo turno, quando deve pescare, pesca tre carte, ne sceglie due da tenere in mano e la rimanente la rimette in cima la mazzo"
 
 class LuckyDuke(Character):
     def __init__(self):
         super().__init__("Lucky Duke", max_lives=4, pick_mod=1)
+        self.desc = "Ogni volta che deve estrarre, prende due carte dal mazzo, sceglie una delle due carte per l'estrazione, infine le scarta entrambe"
 
 class PaulRegret(Character):
     def __init__(self):
         super().__init__("Paul Regret", max_lives=3)
+        self.desc = "Gioca come se avesse una Mustang sempre attiva, nel caso in cui metta in gioco una Mustang 'Reale' l'effetto si somma tranquillamente"
 
 class PedroRamirez(Character):
     def __init__(self):
         super().__init__("Pedro Ramirez", max_lives=4)
+        self.desc = "All'inizio del suo turno, quando deve pescare, può prendere la prima carta dalla cima degli scarti e la seconda dal mazzo"
 
 class RoseDoolan(Character):
     def __init__(self):
         super().__init__("Rose Doolan", max_lives=4)
         self.icon = '🤷‍♀️'
+        self.desc = "Gioca come se avesse un Mirino sempre attivo, nel caso in cui metta in gioco una Mirino 'Reale' l'effetto si somma tranquillamente"
 
 class SidKetchum(Character):
     def __init__(self):
         super().__init__("Sid Ketchum", max_lives=4)
+        self.desc = "Può scartare due carte per recuperare un punto vita anche più volte di seguito a patto di avere carte da scartare, può farlo anche nel turno dell'avversario se starebbe per morire"
 
 class SlabTheKiller(Character):
     def __init__(self):
         super().__init__("Slab The Killer", max_lives=4)
+        self.desc = "Per evitare i suoi Bang servono due Mancato, un eventuale barile vale solo come un Mancato"
+        #vale per tutte le carte bang non solo per la carta che si chiama Bang!
 
 class SuzyLafayette(Character):
     def __init__(self):
         super().__init__("Suzy Lafayette", max_lives=4)
+        self.desc = "Appena rimane senza carte in mano pesca immediatamente una carta dal mazzo"
 
 class VultureSam(Character):
     def __init__(self):
         super().__init__("Vulture Sam", max_lives=4)
+        self.desc = "Quando un personaggio viene eliminato prendi tutte le carte di quel giocatore e aggiungile alla tua mano, sia le carte in mano che quelle in gioco"
 
 class WillyTheKid(Character):
     def __init__(self):
         super().__init__("Willy The Kid", max_lives=4)
+        self.desc = "Questo personaggio può giocare quanti bang vuole nel suo turno"
 
 def all_characters():
     return [
