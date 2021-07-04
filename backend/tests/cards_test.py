b307f2bb34ae5e164ea80cc00af097a3ef470efa
Alberto Xamin <alberto@xamin.it>
2021-07-04 12:20:38 +0200

    add more tests

--- backend/tests/cards_test.py
+++ backend/tests/cards_test.py
@@ -6,6 +6,7 @@ from bang.game import Game
 from bang.players import Player, PendingAction
 from bang.cards import *
 
+# test card Barile
 def test_barile():
     sio = DummySocket()
     g = Game('test', sio)
@@ -41,3 +42,59 @@ def test_barile():
     assert barrel_guy.lives == barrel_guy.max_lives - 1
     assert g.players[g.turn].pending_action == PendingAction.PLAY
 
+#test card Volcanic
+def test_volcanic():
+    sio = DummySocket()
+    g = Game('test', sio)
+    ps = [Player(f'p{i}', f'p{i}', sio) for i in range(3)]
+    for p in ps:
+        g.add_player(p)
+    g.start_game()
+    for p in ps:
+        p.set_character(p.available_characters[0].name)
+        p.character = Character('test_char', 3)
+        p.hand = []
+    volcanic_guy = g.players[g.turn]
+    volcanic_guy.draw('')
+    volcanic_guy.hand = [Volcanic(0,0), Bang(0,0), Bang(0,0)]
+    volcanic_guy.play_card(0)
+    assert isinstance(volcanic_guy.equipment[0], Volcanic)
+    volcanic_guy.play_card(0, g.players[(g.turn+1)%3].name)
+    assert len(volcanic_guy.hand) == 1
+    volcanic_guy.play_card(0, g.players[(g.turn+1)%3].name)
+    assert len(volcanic_guy.hand) == 0
+
+# test card Dinamite
+def test_dinamite():
+    sio = DummySocket()
+    g = Game('test', sio)
+    ps = [Player(f'p{i}', f'p{i}', sio) for i in range(3)]
+    for p in ps:
+        g.add_player(p)
+    g.start_game()
+    for p in ps:
+        p.set_character(p.available_characters[0].name)
+        p.character = Character('test_char', 4)
+        p.hand = []
+    dinamite_guy = g.players[g.turn]
+    dinamite_guy.draw('')
+    dinamite_guy.hand = [Dinamite(0,0)]
+    dinamite_guy.play_card(0)
+    assert isinstance(dinamite_guy.equipment[0], Dinamite)
+    dinamite_guy.end_turn()
+    g.players[g.turn].draw('')
+    g.players[g.turn].hand = []
+    g.players[g.turn].end_turn()
+    g.players[g.turn].draw('')
+    g.players[g.turn].hand = []
+    g.players[g.turn].end_turn()
+    g.deck.cards.insert(0, Dinamite(Suit.HEARTS, 5))
+    dinamite_guy.pick()
+    assert len(dinamite_guy.equipment) == 0
+    dinamite_guy.draw('')
+    dinamite_guy.end_turn()
+    assert len(g.players[g.turn].equipment) == 1
+    g.deck.cards.insert(0, Dinamite(Suit.SPADES, 5))
+    g.players[g.turn].pick()
+    assert len(g.players[g.turn].equipment) == 0
+    assert g.players[g.turn].lives == 1
