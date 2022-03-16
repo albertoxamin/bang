3ebff62a4ac99fde5615f4552291826ea96657b3
Alberto Xamin <alberto.xamin@datadoghq.com>
2022-03-16 09:36:53 +0100

    responsive language and theme buttons

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -176,6 +176,10 @@ html {
 		margin-top: -16pt;
 		zoom: 0.75;
 	}
+	#lang,#theme{
+		max-width: 26pt;
+		min-width: 0;
+	}
 }
 h1,h2,h3,h4,p,span,b,label{
 	font-family: Avenir, Helvetica, Arial, sans-serif;
