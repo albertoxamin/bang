c888632fa7e668573a567848edd4be91ab245765
GM <giulio97.migani@gmail.com>
2023-01-23 08:59:48 +0000

    create the folder if it doesn't exist

--- backend/server.py
+++ backend/server.py
@@ -813,6 +813,8 @@ def save_games():
     while True:
         sio.sleep(2)
         if not save_lock:
+            if not os.path.exists("save"):
+                os.mkdir("save")
             with open('./save/games.pickle', 'wb') as f:
                 pickle.dump([g for g in games if g.started and not g.is_replay and not g.is_hidden], f)
 
